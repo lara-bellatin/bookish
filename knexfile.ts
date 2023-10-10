6a8bb6f4c15ca9f8960d0e5bb0a0bd9545e19832
Lara Bellatin <lara.bellatin@gmail.com>
2023-10-09 21:35:03 -0500

    change prod knex settings

--- knexfile.ts
+++ knexfile.ts
@@ -2,6 +2,22 @@ const { knexSnakeCaseMappers } = require("objection");
 require("dotenv/config");
 
 const knexConfig = {
+  production: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: __dirname + "/db/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds",
+    },
+    ...knexSnakeCaseMappers(),
+  },
   development: {
     client: "postgres",
     connection: {
@@ -47,23 +63,6 @@ const knexConfig = {
     },
     ...knexSnakeCaseMappers(),
   },
-
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: __dirname + "/db/migrations",
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: __dirname + "/db/seeds",
-    },
-    ...knexSnakeCaseMappers(),
-  }
 
 };
 
